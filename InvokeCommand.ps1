5261adcfa0268ae2016d555fc6d48159707984f2
4sanity <sam.bruns0@gmail.com>
2018-01-17 20:20:51 -0700

    Update and rename InvokeCommand to InvokeCommand.ps1

---
+++ InvokeCommand.ps1
@@ -0,0 +1,14 @@
+Add-PSSnapin VMware.VimAutomation.Core
+. 'C:/Program Files (x86)/VMware/Infrastructure/vSphere PowerCLI/Scripts/Initialize-PowerCLIEnvironment.ps1'
+
+###### Initilization done ######
+
+# ENTER vCenter IP address or FQDN:
+$vCenterAddress = "x.x.x.x"
+
+# ENTER vCenter Username:
+$vCenterUser = "example_username"
+
+# ENTER vCenter Password:
+$vPassword = "example_password"
+
