5e941e8ebf181be6d08278e7963e935aa8433dfe
4sanity <sam.bruns0@gmail.com>
2018-01-17 19:35:09 -0700

    Add files via upload

---
+++ AddDatastores.ps1
@@ -0,0 +1,44 @@
+﻿Add-PSSnapin VMware.VimAutomation.Core
+. 'C:/Program Files (x86)/VMware/Infrastructure/vSphere PowerCLI/Scripts/Initialize-PowerCLIEnvironment.ps1'
+
+#######################################################################################################################
+###### Initilization ends here, my script begins here ######
+#######################################################################################################################
+
+# Enter vCenter IP address here:
+$vCenterAddress = "x.x.x.x"
+
+# Enter vCenter Username here:
+$vCenterUser = "example"
+
+# Enter vCenter Password here:
+$vPassword = "example"
+
+#Connecting to vCenter...
+Connect-VIServer $vCenterAddress -User $vCenterUser -Password $vPassword -WarningAction SilentlyContinue
+
+# Datastore prefix name (0 based, modify the index if necessary):
+$DSprefix = "scale_ds_300g."
+
+# Name of the host that the LUNs are exported to:
+$hostName = "x.x.x.x"
+
+#Modify this value if you would like to offset the datastore path position and naming:
+$index = 75
+
+# At this point the LUNs should be manually exported from 3PAR and the host, rescanning HBAs now
+Get-VMHostStorage -VMHost $hostName -RescanAllHba
+
+# Add all of the LUNs that are exported from 3PAR to this host into a variable
+$LUNS = Get-SCSILun -VMHost $hostName -LunType disk -CanonicalName "naa.60002*"
+
+# Loop through all of the LUNs and add them as datastores
+foreach($LUN in $LUNS) {
+    $DSname = $DSprefix + $index
+    $DSpath = $LUN.CanonicalName
+    New-Datastore -VMHost $hostName -Name $DSname -Path $DSpath -Vmfs
+    $index = $index + 1
+}
+
+# Rescan datastores on host after finished
+Get-VMHostStorage -VMHost $hostName -RescanAllHba
---
+++ DeployVMs.ps1
@@ -0,0 +1,64 @@
+﻿Add-PSSnapin VMware.VimAutomation.Core
+. 'C:/Program Files (x86)/VMware/Infrastructure/vSphere PowerCLI/Scripts/Initialize-PowerCLIEnvironment.ps1'
+
+#######################################################################################################################
+###### Initilization ends here, my script begins here ######
+#######################################################################################################################
+
+# Enter vCenter IP address here:
+$vCenterAddress = "x.x.x.x"
+
+# Enter vCenter Username here:
+$vCenterUser = "example"
+
+# Enter vCenter Password here:
+$vPassword = "example"
+
+#Connecting to vCenter...
+Connect-VIServer $vCenterAddress -User $vCenterUser -Password $vPassword -WarningAction SilentlyContinue
+
+# Change these variables based on the VMs you would like deploy:
+# Number of VMs:
+$VMquantity = 75
+
+# Host to deploy the VMs to:
+$hostName = "x.x.x.x"
+
+# VM prefix name (0 based, modify the index if necessary):
+$VMprefix = "scale"
+
+# Name of the VM template / clone to use in the vCenter:
+#$template = "rhel7-vd"
+$clone = Get-VM rhel7-vd
+
+# Datastore prefix name that the VM will deployed on (0 based, modify the index if necessary):
+$datastorePrefix = "scale_ds_300g."
+
+# Name of the cluster to deploy to (only if datastores can be accessed from all hosts):
+#$clusterName = "Cluster1"
+
+#Modify this value if you would like to offset the VM deployment:
+$index = 75
+
+#File path to the OVF if this is the deployment method:
+#$sourcePath = "C:\Users\Administrator\Desktop\rhel7-vd.ovf"
+
+# Deploys in a loop, additional VM cloning starts everytime one finishes
+1..$VMquantity | foreach {
+    $VMname = $VMprefix + $index
+    $datastoreName = $datastorePrefix + $index
+    
+    # For OVF
+    #Import-VApp -Source $sourcePath VMHost -$hostName -Name $VMname -Datastore $datastoreName -DiskStorageFormat Thin -ErrorAction stop
+    
+    # For templates
+    #New-vm -Name $VMname -VMHost $hostName -Template $template -Datastore $datastoreName -DiskStorageFormat Thin -ErrorAction stop
+    # by cluster instead of host:
+    #New-vm -Name $VMname -ResourcePool $clusterName -Template $template -Datastore $datastoreName -DiskStorageFormat Thin -ErrorAction stop
+
+    # For cloning
+    $DeployVM = New-VM -Name $VMname -VM $clone -Datastore $datastoreName -DiskStorageFormat Thin -VMHost $hostName -ErrorAction stop
+    
+    $index = $index + 1
+    Start-VM $VMname -Confirm:$false
+}
