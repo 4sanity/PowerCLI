3093e5a7a4b2ce606be52862d88b241f64e88418
4sanity <sam.bruns0@gmail.com>
2018-01-17 20:15:45 -0700

    Update AddDatastores.ps1

--- AddDatastores.ps1
+++ AddDatastores.ps1
@@ -1,35 +1,36 @@
 ﻿Add-PSSnapin VMware.VimAutomation.Core
 . 'C:/Program Files (x86)/VMware/Infrastructure/vSphere PowerCLI/Scripts/Initialize-PowerCLIEnvironment.ps1'
 
-#######################################################################################################################
-###### Initilization ends here, my script begins here ######
-#######################################################################################################################
+###### Initilization done ######
 
-# Enter vCenter IP address here:
+# ENTER vCenter IP address or FQDN:
 $vCenterAddress = "x.x.x.x"
 
-# Enter vCenter Username here:
-$vCenterUser = "example"
+# ENTER vCenter Username:
+$vCenterUser = "example_username"
 
-# Enter vCenter Password here:
-$vPassword = "example"
+# ENTER vCenter Password:
+$vPassword = "example_password"
+
+# ENTER datastore prefix:
+$DSprefix = "example_datastore_prefix_name"
+
+# ENTER the name of the host that the LUNs are exported to:
+$hostName = "x.x.x.x"
+
+# MODIFY this value if you would like to offset the datastore naming pattern:
+$index = 0
+
+
+###### Done with user modifications ######
 
 #Connecting to vCenter...
 Connect-VIServer $vCenterAddress -User $vCenterUser -Password $vPassword -WarningAction SilentlyContinue
 
-# Datastore prefix name (0 based, modify the index if necessary):
-$DSprefix = "scale_ds_300g."
-
-# Name of the host that the LUNs are exported to:
-$hostName = "x.x.x.x"
-
-#Modify this value if you would like to offset the datastore path position and naming:
-$index = 75
-
 # At this point the LUNs should be manually exported from 3PAR and the host, rescanning HBAs now
 Get-VMHostStorage -VMHost $hostName -RescanAllHba
 
-# Add all of the LUNs that are exported from 3PAR to this host into a variable
+# Add all of the LUNs that are exported from 3PAR to this host into a variable, filtered for 3PAR LUNs, change if necessary
 $LUNS = Get-SCSILun -VMHost $hostName -LunType disk -CanonicalName "naa.60002*"
 
 # Loop through all of the LUNs and add them as datastores
