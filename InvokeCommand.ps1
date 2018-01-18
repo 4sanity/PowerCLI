338b1ad635dc90348db66f9043048670998f251c
4sanity <sam.bruns0@gmail.com>
2018-01-17 20:28:22 -0700

    Update InvokeCommand.ps1

--- InvokeCommand.ps1
+++ InvokeCommand.ps1
@@ -12,3 +12,19 @@ $vCenterUser = "example_username"
 # ENTER vCenter Password:
 $vPassword = "example_password"
 
+# ENTER the name of the host that the LUNs are exported to:
+$hostName = "x.x.x.x"
+
+# ENTER the script/command to be invoked:
+$
+
+# MODIFY this value if you would like to offset the VMs this command will be run on:
+$index = 0
+
+
+###### Done with user modifications ######
+
+#Connecting to vCenter...
+Connect-VIServer $vCenterAddress -User $vCenterUser -Password $vPassword -WarningAction SilentlyContinue
+
+#Loop through VMs
